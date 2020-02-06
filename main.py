27622a6522fb8d32c5adce40b5f02c241b977e61
Santiago Dangond <santiago.dangond18@gmail.com>
2020-02-06 09:11:30 -0500

    limpiando codigo. cambio de funciones en los botones y operaciones

--- main.py
+++ main.py
@@ -1,113 +1,52 @@
 from tkinter import * 
 
-from model.Calculadora import *
-from cal_values import *
-
 # Var Globales
 num_total = 0
 var_first = True # variable que indica si es la primera operacion en realizarse
 TAM_BOTON = 50
 final_operation = ''
 
+root = Tk()
+root.config(bd=15)
+root.resizable(0,0)
+root.title('Calculadora')
 
-#methods
-def cero():
-    result.set(result.get() + '0')
-    
+result = StringVar()
 
-def uno():
-    result.set(result.get() + '1')
-    
-    
-def dos():
-    result.set(result.get() + '2')
-    
-def tres():
-    result.set(result.get() + '3')
-    
-def cuatro():
-    result.set(result.get() + '4')
-    
-def cinco():
-    result.set(result.get() + '5')
-    
-def seis():
-    result.set(result.get() + '6')
-    
-def siete():
-    result.set(result.get() + '7')
-    
-def ocho():
-    result.set(result.get() + '8')
-    
-def nueve():
-    result.set(result.get() + '9')
-    
+frame = Frame(root , width=480 , height=340)
+frame.pack(fill='both' , expand=1)
+frame.config(bg='lightblue')
+frame.config(bd=25)
 
-    
-def concat():
-    """ funcion que concatena la lista de numeros y lo escribe en el label del resultado """
-    pass
+def set_num(num):
+    result.set(result.get() + str(num))
 
-
-def clear():
-    """ Clear the labels """ 
-    num1.set(' ')
-    num2.set(' ')
-    var_first = True
-
-def sumar():
-    """Pues suma """
+def operation(op):
     global num_total
     global final_operation
     global var_first
-    num_total += int(result.get())
-    print('n t {}'.format(num_total))
-    final_operation = '+'
-    result.set("")
-    var_first = False
+    final_operation = op
+    print(op)
+    if var_first:
+        num_total = int(result.get())
+        var_first = False
+    else:
+        if op == '+':
+            num_total += int(result.get())
+            print('nt {}'.format(num_total))
+        if op == '-':
+            num_total -= int(result.get())
+            print('nt {}'.format(num_total))
+        if op == '*':
+            num_total = num_total * int(result.get())
+            print('nt {}'.format(num_total))
+        if op == '/':
+            num_total = int( num_total / int(result.get()) ) 
+            print('nt {}'.format(num_total))
+    result.set("")  
         
-def restar():
-    """pues resta """    
-    global num_total
-    global final_operation
-    global var_first
-    if var_first:
-        num_total = int(result.get())
-        var_first = False
-    else:
-        num_total -= int(result.get())
-    final_operation = '-'
-    print('n t {}'.format(num_total))
-    result.set("")
-
-def multiplicar():
-    """pues multiplica """    
-    global num_total
-    global var_first
-    global final_operation
-    if var_first:
-        num_total = int(result.get())
-        var_first = False
-    else:
-        num_total = num_total * int(result.get())
-    print('n t {}'.format(num_total))
-    final_operation = '*'
-    result.set("")
         
-def dividir():
-    """pues divide """    
-    global num_total
-    global var_first
-    global final_operation
-    if var_first:
-        num_total = int(result.get())
-        var_first = False
-    else:
-        num_total = int( num_total / int(result.get()) ) 
-    print('n t {}'.format(num_total))
-    final_operation = '/'
-    result.set("")
+        
 
 def cls_method():
     """clean all data """ 
@@ -122,68 +61,37 @@ def igual():
     global num_total
     global final_operation
     global var_first
-    if final_operation == '+':
-        sumar()
-    if final_operation == '-':
-        restar()
-    if final_operation == '*':
-        multiplicar()
-    if final_operation == '/':
-        dividir() 
+    operation(final_operation)
     result.set(str(num_total))
     var_first = True
     num_total = 0
 
 
 
-root = Tk()
-root.config(bd=15)
-root.resizable(0,0)
-root.title('Calculadora')
-#tres variables para manejar los numeros y el resultado
-num1 = StringVar()
-num2 = StringVar()
-result = StringVar()
-
-#Label(root , text='Num 1').grid(row=0,column=0)
-#Entry(root , justify=CENTER , textvariable=num1 ).pack(side=TOP)
-
-#Label(root , text='Num 2').grid(row=1,column=0)
-#Entry(root , justify=CENTER , textvariable=num2 ).grid(row=1,column=1)
-#Label(root).grid(row=1) #separador
-
-#Label(root).grid(row=2) #separador
-frame = Frame(root , width=480 , height=340)
-frame.pack(fill='both' , expand=1)
-frame.config(bg='lightblue')
-frame.config(bd=25)
-
 
 #Elements
 Entry(frame , state=DISABLED, textvariable=result).place(x=15, y=10, width=400 , height=30)
 
+Button(root, text='1' , command=lambda:set_num(1)).place(x=40, y=100,width=TAM_BOTON , height=TAM_BOTON)
+Button(root, text='2' , command=lambda:set_num(2)).place(x=40, y=160,width=TAM_BOTON , height=TAM_BOTON)
+Button(root, text='3' , command=lambda:set_num(3)).place(x=40,y=220,width=TAM_BOTON , height=TAM_BOTON)
+
+Button(root, text='4' , command=lambda:set_num(4)).place(x=110,y=100,width=TAM_BOTON , height=TAM_BOTON)
+Button(root, text='5' , command=lambda:set_num(5)).place(x=110,y=160,width=TAM_BOTON , height=TAM_BOTON)
+Button(root, text='6' , command=lambda:set_num(6)).place(x=110,y=220,width=TAM_BOTON , height=TAM_BOTON)
+
+Button(root, text='7' , command=lambda:set_num(7)).place(x=180,y=100,width=TAM_BOTON , height=TAM_BOTON)
+Button(root, text='8' , command=lambda:set_num(8)).place(x=180,y=160,width=TAM_BOTON , height=TAM_BOTON)
+Button(root, text='9' , command=lambda:set_num(9)).place(x=180,y=220,width=TAM_BOTON , height=TAM_BOTON)
+
+Button(root, text='0' , command=lambda:set_num(0)).place(x=40,y=280,width=190 , height=TAM_BOTON)
 
 
-Button(root, text='1' , command=uno).place(x=40, y=100,width=TAM_BOTON , height=TAM_BOTON)
-Button(root, text='2' , command=dos).place(x=40, y=160,width=TAM_BOTON , height=TAM_BOTON)
-Button(root, text='3' , command=tres).place(x=40,y=220,width=TAM_BOTON , height=TAM_BOTON)
+Button(root, text='+' , command=lambda:operation("+")).place(x=260,y=100,width=TAM_BOTON , height=TAM_BOTON)
+Button(root, text='-' , command=lambda:operation("-")).place(x=260,y=160,width=TAM_BOTON , height=TAM_BOTON)
+Button(root, text='*' , command=lambda:operation("*")).place(x=260,y=220,width=TAM_BOTON , height=TAM_BOTON)
 
-Button(root, text='4' , command=cuatro).place(x=110,y=100,width=TAM_BOTON , height=TAM_BOTON)
-Button(root, text='5' , command=cinco).place(x=110,y=160,width=TAM_BOTON , height=TAM_BOTON)
-Button(root, text='6' , command=seis).place(x=110,y=220,width=TAM_BOTON , height=TAM_BOTON)
-
-Button(root, text='7' , command=siete).place(x=180,y=100,width=TAM_BOTON , height=TAM_BOTON)
-Button(root, text='8' , command=ocho).place(x=180,y=160,width=TAM_BOTON , height=TAM_BOTON)
-Button(root, text='9' , command=nueve).place(x=180,y=220,width=TAM_BOTON , height=TAM_BOTON)
-
-Button(root, text='0' , command=cero).place(x=40,y=280,width=190 , height=TAM_BOTON)
-
-
-Button(root, text='+' , command=sumar).place(x=260,y=100,width=TAM_BOTON , height=TAM_BOTON)
-Button(root, text='-' , command=restar).place(x=260,y=160,width=TAM_BOTON , height=TAM_BOTON)
-Button(root, text='*' , command=multiplicar).place(x=260,y=220,width=TAM_BOTON , height=TAM_BOTON)
-
-Button(root, text='/' , command=dividir).place(x=330,y=100,width=TAM_BOTON , height=TAM_BOTON)
+Button(root, text='/' , command=lambda:operation("/")).place(x=330,y=100,width=TAM_BOTON , height=TAM_BOTON)
 
 Button(root, text='Cls' , command=cls_method).place(x=330,y=160,width=TAM_BOTON , height=TAM_BOTON + 60)
 Button(root, text='=' , command=igual).place(x=260,y=280,width=190 , height=TAM_BOTON)
